b5a965d834ade969c0382c998438830cc40d7dce
William Emerison Six <billsix@gmail.com>
2017-04-19 23:34:31 -0400

    nuklear flags must match

--- src/common.h
+++ src/common.h
@@ -28,6 +28,13 @@
 #include <GL/glew.h>
 #include <assert.h>
 
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
 #include "../contrib/nuklear/nuklear.h"
 #include "../contrib/nuklear/nuklear_sdl_gl3.h"
 
